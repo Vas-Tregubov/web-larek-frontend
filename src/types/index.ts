22129e56ff7f37c05aa8def0addb7d1d88509a5f
Vas Tregubov <vastregubov@gmail.com>
2024-09-13 17:14:15 +0300

    docs(types/index.ts): Add core interfaces for project classes
    1. Introduced IProduct and IOrder interfaces to standardize data
       structures for products and orders.
    2. Defined IModal interface to outline essential modal window methods
       and properties.
    3. Added ICard interface for defining methods and properties of the
       Card class.

---
+++ src/types/index.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, IEvents } from '../components/base/events';
+
+// Интерфейс карточки товара
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: string;
+	price: number | null;
+}
+
+// Интерфейс заказа
+export interface IOrder {
+	payment: string;
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+
+// Интерфейс для базового класса Modal
+export interface IModal {
+	modal: HTMLElement;
+	events: EventEmitter;
+
+	open(): void;
+	close(): void;
+	setEventListeners(): void;
+}
+
+// Интерфейс для класса Card
+export interface ICard {
+	element: HTMLElement;
+	events: IEvents;
+
+	render(productData: IProduct): HTMLElement;
+	update(productData: IProduct): void;
+	openProductModal(): void;
+}
