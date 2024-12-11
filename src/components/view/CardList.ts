9fcecae8711e65d1c1f52db00d269e8b6e63ae30
Vas Tregubov <vastregubov@gmail.com>
2024-12-11 14:18:08 +0300

    feat(CardList.ts) Implement CardList class and add documentation
    Extends CardBasket class to manage product category and image.
    Constructor accepts container element and optional click event handler.
    Contains properties for category and image elements.
    Implements setters for category, image source, and selected state.
    Provides documentation for class structure and methods.

---
+++ src/components/view/CardList.ts
@@ -0,0 +1,31 @@
+import { CardBasket } from './CardBasket';
+import { ensureElement } from '../../utils/utils';
+import { CDN_URL } from '../../utils/constants';
+import { categoryMap, TProductCategory } from '../../types/index';
+
+export class CardList extends CardBasket {
+	protected category: HTMLElement;
+	protected image: HTMLImageElement;
+
+	constructor(container: HTMLElement, onClick?: (event: MouseEvent) => void) {
+		super(container, onClick);
+
+		this.category = ensureElement<HTMLElement>('.card__category', container);
+		this.image = ensureElement<HTMLImageElement>('.card__image', container);
+	}
+
+	set setCategory(category: TProductCategory) {
+		this.category.textContent = category;
+		this.category.classList.add(categoryMap[category]);
+	}
+
+	set setImage(image: string) {
+		this.image.src = CDN_URL + image;
+	}
+
+	set setSelected(value: boolean) {
+		if (value) {
+			this.button.disabled = value;
+		}
+	}
+}
