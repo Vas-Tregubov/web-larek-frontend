2dc068ece3fdebd5971abb22f04a06d9239d8ef8
Vas Tregubov <vastregubov@gmail.com>
2024-12-12 16:53:03 +0300

    feat(CustomerData.ts) Implement CustomerData class with validation logic
    Created CustomerData class for handling user order data such as payment, address, email, and phone.
    Added methods for setting order details, validating contact and order information, and emitting events upon successful validation.
    Implemented setOrder to update fields and trigger validation.
    Added getOrderDetails to retrieve user data in the format Partial<IOrder>.
    Provided validation for order details (payment and address) and contact details (email and phone).
    Updated event emissions based on validation status for contacts and order readiness.

---
+++ src/components/model/CostumerData.ts
@@ -0,0 +1,64 @@
+import { IOrder } from '../../types/index';
+import { Model } from '../base/Model';
+
+export class CostumerData extends Model<IOrder> {
+	protected payment: string = '';
+	protected address: string = '';
+	protected email: string = '';
+	protected phone: string = '';
+
+	validationErrors: Partial<Record<keyof IOrder, string>> = {};
+
+	set setOrder({ field, value }: { field: keyof IOrder; value: string }) {
+		(this as any)[field] = value;
+
+		if (this.validateContactDetails()) {
+			this.events.emit('contacts:ready', this.getOrderDetails());
+		}
+
+		if (this.validateOrderDetails()) {
+			this.events.emit('order:ready', this.getOrderDetails());
+		}
+	}
+
+	getOrderDetails(): Partial<IOrder> {
+		return {
+			payment: this.payment,
+			address: this.address,
+			email: this.email,
+			phone: this.phone,
+		};
+	}
+
+	validateOrderDetails(): boolean {
+		const errors: Partial<Record<keyof IOrder, string>> = {};
+
+		if (!this.address) {
+			errors.address = 'Address is required';
+		}
+		if (!this.payment) {
+			errors.payment = 'Payment method is required';
+		}
+
+		this.validationErrors = errors;
+		this.events.emit('orderFormErrors:change', this.validationErrors);
+
+		return Object.keys(errors).length === 0;
+	}
+
+	validateContactDetails(): boolean {
+		const errors: Partial<Record<keyof IOrder, string>> = {};
+
+		if (!this.email) {
+			errors.email = 'Email is required';
+		}
+		if (!this.phone) {
+			errors.phone = 'Phone number is required';
+		}
+
+		this.validationErrors = errors;
+		this.events.emit('contactsFormErrors:change', this.validationErrors);
+
+		return Object.keys(errors).length === 0;
+	}
+}
