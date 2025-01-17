cc50c2b7fa3cd458f2779dcdef62790eb13df65e
Vas Tregubov <vastregubov@gmail.com>
2025-01-18 01:04:04 +0300

    refactor(Page.ts) Refactor Page class and update documentation
     - Renamed class fields for better clarity and consistency.
     - Updated method names to match naming conventions.
     - Improved documentation to reflect new variable names.
     - Simplified event handling and streamlined DOM element access.

--- src/components/view/Basket.ts
+++ src/components/view/Basket.ts
@@ -9,28 +9,28 @@ export interface IBasket {
 
 export class Basket extends Component<IBasket> {
 	protected _list: HTMLElement;
-	protected _total: HTMLElement;
-	protected _button: HTMLButtonElement;
+	protected totalPrice: HTMLElement;
+	protected basketButton: HTMLButtonElement;
 
 	constructor(protected container: HTMLElement, protected events: IEvents) {
 		super(container);
 		this.events = events;
 
 		this._list = ensureElement<HTMLElement>('.basket__list', container);
-		this._total = ensureElement<HTMLElement>('.basket__price', container);
-		this._button = ensureElement<HTMLButtonElement>(
+		this.totalPrice = ensureElement<HTMLElement>('.basket__price', container);
+		this.basketButton = ensureElement<HTMLButtonElement>(
 			'.basket__button',
 			container
 		);
 
-		this._button.addEventListener('click', () => {
+		this.basketButton.addEventListener('click', () => {
 			this.events.emit('order:open', {});
 		});
 	}
 
 	set total(value: number) {
-		this._total.textContent = value + ' синапсов';
-		this.setDisabled(this._button, value === 0);
+		this.totalPrice.textContent = value + ' синапсов';
+		this.setDisabled(this.basketButton, value === 0);
 	}
 
 	set list(items: HTMLElement[]) {
--- src/components/view/Page.ts
+++ src/components/view/Page.ts
@@ -4,43 +4,43 @@ import { Component } from '../base/Component';
 import { IEvents } from '../base/events';
 
 export class Page extends Component<ICatalog> {
-	protected _counter: HTMLElement;
-	protected _catalog: HTMLElement;
-	protected _basket: HTMLButtonElement;
-	protected _wrapper: HTMLElement;
+	protected basketCounter: HTMLElement;
+	protected gallery: HTMLElement;
+	protected basketButton: HTMLButtonElement;
+	protected wrapper: HTMLElement;
 
 	constructor(protected container: HTMLElement, protected events: IEvents) {
 		super(container);
 		this.events = events;
-		this._counter = ensureElement<HTMLButtonElement>(
+		this.basketCounter = ensureElement<HTMLButtonElement>(
 			'.header__basket-counter',
 			container
 		);
-		this._catalog = ensureElement<HTMLElement>('.gallery', container);
-		this._basket = ensureElement<HTMLButtonElement>(
+		this.gallery = ensureElement<HTMLElement>('.gallery', container);
+		this.basketButton = ensureElement<HTMLButtonElement>(
 			'.header__basket',
 			container
 		);
-		this._wrapper = ensureElement<HTMLElement>('.page__wrapper', container);
+		this.wrapper = ensureElement<HTMLElement>('.page__wrapper', container);
 
-		this._basket.addEventListener('click', () => {
+		this.basketButton.addEventListener('click', () => {
 			this.events.emit('basket:open', {});
 		});
 	}
 
 	set counter(counter: number) {
-		this._counter.textContent = String(counter);
+		this.basketCounter.textContent = String(counter);
 	}
 
 	set catalog(items: HTMLElement[]) {
-		this._catalog.replaceChildren(...items);
+		this.gallery.replaceChildren(...items);
 	}
 
 	set locked(value: boolean) {
 		if (value) {
-			this._wrapper.classList.add('page__wrapper_locked');
+			this.wrapper.classList.add('page__wrapper_locked');
 		} else {
-			this._wrapper.classList.remove('page__wrapper_locked');
+			this.wrapper.classList.remove('page__wrapper_locked');
 		}
 	}
 }
